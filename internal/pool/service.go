e1f72cfb2b3cdbdd32606309c1f15306dddd3796
BlueDragon747 <5333740+BlueDragon747@users.noreply.github.com>
2026-06-24 12:57:17 +0100

    log noise
    reduce log noise and reset interval

--- internal/pool/service.go
+++ internal/pool/service.go
@@ -335,7 +335,7 @@ func (s *Service) parentHeartbeatLoop(ctx context.Context) {
 }
 
 func (s *Service) checkParentHeartbeat() {
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	raw, err := s.parent.Call(ctx, "getblockchaininfo")
 	cancel()
 	if err != nil {
